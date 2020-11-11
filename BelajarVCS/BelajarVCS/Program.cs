be061354faf91cb7c7faa1ce16753d43b36cf1bb
antonindra2 <indraantonius11@gmail.com>
2020-11-11 11:50:42 +0700

    Add

---
+++ BelajarVCS/BelajarVCS/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BelajarVCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
